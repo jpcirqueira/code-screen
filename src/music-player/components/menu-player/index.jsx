94ed079e9761f52b85902ef5dc349a6be8049880
jpcirqueira <jpcirqueira81@gmail.com>
2024-01-11 21:53:02 -0300

    create MenuPlayer component

---
+++ src/music-player/components/menu-player/index.jsx
@@ -0,0 +1,30 @@
+import PlayBackIcon from '../../images/play-back.svg';
+import PlayForwardIcon from '../../images/play-forward.svg';
+import PlayIcon from '../../images/play.svg';
+
+import './styles.css';
+
+const MenuPlayer = ({
+    startTime,
+    finishTime
+}) => {
+
+    return (
+        <div>
+            <div className="menu-player-actions">
+                <img src={PlayBackIcon} />
+                <img src={PlayIcon} />
+                <img src={PlayForwardIcon} />
+            </div>
+            <div className="menu-player-progress">
+                <div className="menu-player-progress-bar"/>
+                <div className="menu-player-progress-time">
+                    <p>{startTime}</p>
+                    <p>{finishTime}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export { MenuPlayer };
